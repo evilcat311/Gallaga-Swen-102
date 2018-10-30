d846662514774d70c0ce1576e2581653a9c2b759
pgwoods190 <pgwoods190@gmail.com>
2018-10-30 14:45:44 -0400

    Add files via upload

---
+++ MenuCtrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuCtrl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (Input.anyKey)
+        {
+            Debug.Log("A key or mouse click has been detected");
+            SceneManager.LoadScene("InGameScene", LoadSceneMode.Single);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 10; //The default speed for the player
+
+    public Rigidbody2D playerFrame; //reference to the rigid body (collidable) around the player
+
+	// Use this for initialization
+	void Start () {
+        playerFrame = this.GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //Input.gyro.enabled = true;
+        //float initialOrientationX = Input.gyro.rotationRateUnbiased.x;
+        //transform.position = new Vector2(0.0f, 0.0f);
+        //MovePlayer(initialOrientationX);
+    }
+
+    public void MovePlayer(float initialOrientationX)
+    {
+        //Controls
+        //playerFrame.AddForce (initialOrientationX * moveSpeed, 0.0f);
+
+        //Animations???
+
+        //Physics
+    }
+
+    public void Shoot()
+    {
+        //Shooting code
+    }
+}
